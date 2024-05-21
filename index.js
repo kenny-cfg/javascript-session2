1e20c956f6672cc98ef5c0ec07e0e2552d61bf3c
Kenny Hung (he) <kenny.cfg@outlook.com>
2024-05-21 19:20:13 +0100

    Example if-else clause

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-let price = 100
-const discount = 20
-
-console.log("Price: " + price)
-
-price = price * ((100 - discount) / 100)
-
-console.log("New price: " + price)
+const response = prompt('Books vs movies')
+if (response === 'books') {
+  console.log('BOOKS')
+} else {
+  console.log('FILMS')
+}
