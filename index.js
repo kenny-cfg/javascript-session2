4dd8a612adb38470ae1dd290398f7e73383f22e7
Kenny Hung (he) <kenny.cfg@outlook.com>
2024-05-21 20:34:19 +0100

    Example of bad scope

--- index.js
+++ index.js
@@ -1,25 +1,8 @@
-function banner(messageInsideFunction) {
-  console.log("_________________________")
-  console.log(messageInsideFunction)
-  console.log("_________________________")
-  messageInsideFunction = "I am done with the message"
-  console.log(messageInsideFunction)
-  console.log('Message: ', message)
+let result = null
+
+function addEmojies(message, emoji) {
+  let result = emoji + " " + message + " " + emoji
 }
 
-const bannerLambda = (messageInsideLambdaFunction) => {
-  console.log("_________________________")
-  console.log(messageInsideLambdaFunction)
-  console.log("_________________________")
-  console.log('Message: ', message)
-}
-
-const bannerFunction = function(message) {
-  console.log("_________________________")
-  console.log(message)
-  console.log("_________________________")
-}
-
-const message = "Hi"
-bannerLambda(message)
-console.log(message)
+addEmojies("hello", "WAVE")
+console.log(result)
